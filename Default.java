d2893cf4975445304659d07e81b57af3be3d035d
Muhammad Adnan <131403461+Addii61@users.noreply.github.com>
2023-10-15 03:00:46 +0500

    Add files via upload

---
+++ Default.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkgdefault.constructor;
+
+/**
+ *
+ * @author cui
+ */
+public class Default {
+    private int a;
+    private String b;
+    public void Display(){
+        System.out.printf("%d:%s",a,b);
+    }      
+}
